68bad5014444408579bd35d310effed028c8dc82
Xavier_Jimenez <xavierjp1220@gmail.com>
2023-08-23 02:09:48 -0400

    Initial commit: Proyect Creation with Basic Structure

---
+++ QuizTime_Game/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizTime_Game;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ QuizTime_Game/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace QuizTime_Game;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
